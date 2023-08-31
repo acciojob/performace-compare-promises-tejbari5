d7726bc8639f3354a9f79d4a6e736979e2add1f6
Tej Bari <136843098+tejbari5@users.noreply.github.com>
2023-08-31 20:05:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,5 +11,48 @@ const apiUrls = [
   "https://jsonplaceholder.typicode.com/todos/9",
   "https://jsonplaceholder.typicode.com/todos/10",
 ];
+function fetchData(){
 
+  const fetchPromises=apiUrls.map((url)=>fetch(url));
+
+    // Start the timer for Promise.all
+  const startTimeAll=performance.now();
+
+   Promise.all(fetchPromises)
+   .then(()=>{
+
+     // Calculate the time taken for Promise.all
+    const endTimeAll=performance.now();
+    const timeTakenAll=endTimeAll-startTimeAll;
+
+    // now we will put this result in table data
+
+    let outputAll=document.getElementById('output-all');
+    outputAll.innerText=`${timeTakenAll}ms`;
+   })
+   .catch((error)=>{
+    console.log('something went wrong in Promise.all',error);
+   })
+   
+     // Start the timer for Promise.any
+     
+     const startTimeAny=performance.now();
+     
+     Promise.any(fetchPromises)
+     .then(()=>{
+
+         // Calculate the time taken for Promise.any
+         const endTimeAny=performance.now();
+         const timeTakenAny=endTimeAny-startTimeAny;
+
+          // now we will put this result in table data
+          let outputAny =document.getElementById('output-any');
+          outputAny.innerText=`${timeTakenAny}ms`;
+     })
+     .catch((error)=>{
+      console.log('something went wrong n Promise.any',error);
+     })
+}
+
+fetchData();
 // You can write your code here
