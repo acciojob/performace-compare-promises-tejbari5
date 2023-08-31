b7cf4eb650aedee7563bd5d9b78690e3864dd1d5
Tej Bari <136843098+tejbari5@users.noreply.github.com>
2023-08-31 20:10:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,58 +1,54 @@
 // Array of API URLs to fetch data from
 const apiUrls = [
-  "https://jsonplaceholder.typicode.com/todos/1",
-  "https://jsonplaceholder.typicode.com/todos/2",
-  "https://jsonplaceholder.typicode.com/todos/3",
-  "https://jsonplaceholder.typicode.com/todos/4",
-  "https://jsonplaceholder.typicode.com/todos/5",
-  "https://jsonplaceholder.typicode.com/todos/6",
-  "https://jsonplaceholder.typicode.com/todos/7",
-  "https://jsonplaceholder.typicode.com/todos/8",
-  "https://jsonplaceholder.typicode.com/todos/9",
-  "https://jsonplaceholder.typicode.com/todos/10",
+    "https://jsonplaceholder.typicode.com/todos/1",
+    "https://jsonplaceholder.typicode.com/todos/2",
+    "https://jsonplaceholder.typicode.com/todos/3",
+    "https://jsonplaceholder.typicode.com/todos/4",
+    "https://jsonplaceholder.typicode.com/todos/5",
+    "https://jsonplaceholder.typicode.com/todos/6",
+    "https://jsonplaceholder.typicode.com/todos/7",
+    "https://jsonplaceholder.typicode.com/todos/8",
+    "https://jsonplaceholder.typicode.com/todos/9",
+    "https://jsonplaceholder.typicode.com/todos/10",
 ];
-function fetchData(){
 
-  const fetchPromises=apiUrls.map((url)=>fetch(url));
+// You can write your code here
 
-    // Start the timer for Promise.all
-  const startTimeAll=performance.now();
-
-   Promise.all(fetchPromises)
-   .then(()=>{
-
-     // Calculate the time taken for Promise.all
-    const endTimeAll=performance.now();
-    const timeTakenAll=endTimeAll-startTimeAll;
-
-    // now we will put this result in table data
-
-    let outputAll=document.getElementById('output-all');
-    outputAll.innerText=`${timeTakenAll}ms`;
-   })
-   .catch((error)=>{
-    console.log('something went wrong in Promise.all',error);
-   })
-   
-     // Start the timer for Promise.any
-     
-     const startTimeAny=performance.now();
-     
-     Promise.any(fetchPromises)
-     .then(()=>{
-
-         // Calculate the time taken for Promise.any
-         const endTimeAny=performance.now();
-         const timeTakenAny=endTimeAny-startTimeAny;
-
-          // now we will put this result in table data
-          let outputAny =document.getElementById('output-any');
-          outputAny.innerText=`${timeTakenAny}ms`;
-     })
-     .catch((error)=>{
-      console.log('something went wrong n Promise.any',error);
-     })
+// Function to fetch data from an API
+async function fetchData(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
 }
 
-fetchData();
-// You can write your code here
+// Function to measure time taken by a promise 
+async function measurePromiseTime(promiseFunction) {
+    const startTime = performance.now();
+    await promiseFunction();
+    const endTime = performance.now();
+    return endTime - startTime;
+}
+
+// Function to fetch data using Promise.all()
+async function fetchAllData() {
+    const promises = apiUrls.map(url => fetchData(url));
+    await Promise.all(promises);
+}
+
+// Function to fetch data using Promise.any()
+async function fetchAllData() {
+    const promises = apiUrls.map(url => fetchData(url));
+    await Promise.any(promises);
+}
+
+// Update the table with results
+async function updateTable() {
+    const allTime = await measurePromiseTime(fetchAllData);
+    const anyTime = await measurePromiseTime(fetchAllData);
+
+    document.getElementById('output-all').textContent = allTime.toFixed(2) + ' ms';
+    document.getElementById('output-any').textContent = allTime.toFixed(2) + ' ms';
+}
+
+// Call updateTable() when the page loads
+window.addEventListener('load', updateTable);
